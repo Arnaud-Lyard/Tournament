f9dea568d61ef249d6f454dd55cfe73bc8e0849c
Arnaud-Lyard <arnaud.lyard@gmail.com>
2024-04-24 20:19:17 +0200

    🔥 clean project

--- server/src/user/dto/user.dto.ts
+++ server/src/user/dto/user.dto.ts
@@ -9,8 +9,4 @@ export interface IUserUpdateDto {
   id: string;
   pseudo: string;
   email: string;
-  avatar?: string | null;
-  esl?: string | null;
-  twitter?: string | null;
-  stormgate?: string | null;
 }
--- server/src/user/routes/user.routes.ts
+++ server/src/user/routes/user.routes.ts
@@ -1,24 +1,17 @@
 import express from 'express';
 import { authenticateUser } from '../../middleware/authenticateUser';
-import { uploadFile } from '../../middleware/uploadFile';
 import {
   getMeHandler,
-  getTeamUsersHandler,
   getUserHandler,
   updateUserHandler,
-  uploadUserImageHandler,
 } from '../controller/user.controller';
 
 const router = express.Router();
 
 router.get('/', authenticateUser, getUserHandler);
 
-router.post('/upload', authenticateUser, uploadFile, uploadUserImageHandler);
-
-router.get('/team', getTeamUsersHandler);
-
 router.get('/me', getMeHandler);
 
-router.post('/update', authenticateUser, uploadFile, updateUserHandler);
+router.post('/update', authenticateUser, updateUserHandler);
 
 export default router;
