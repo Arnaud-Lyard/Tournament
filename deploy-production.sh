e5055eaf58ff0aa5a806f79ea823d9ab2e6ee201
Arnaud Lyard <arnaud.lyard@gmail.com>
2024-06-12 20:24:36 +0200

    deploy

--- deploy-production.sh
+++ deploy-production.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-git fetch origin && git reset --hard origin/main && git clean -f -d && \
-docker compose -f docker-compose.production.yml down && \
-docker compose -f docker-compose.production.yml pull && \
-docker compose -f docker-compose.production.yml --env-file .env.production up -d;
+git fetch origin && git reset --hard origin/main && git clean -f -d && git pull && \ 
+docker compose -f docker-compose.production.yml up --build -d
