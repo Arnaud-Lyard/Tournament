878ed9a17965125d8462d8d3956dd723cb258680
Arnaud Lyard <arnaud.lyard@gmail.com>
2024-09-28 07:51:01 +0200

    🐛 Fix notification route

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -209,7 +209,7 @@ export default function Navbar({ params }: { params: { lang: string } }) {
                                       <MenuItem key={post.id}>
                                         {({ active }) => (
                                           <Link
-                                            href={`/post/${post.slug}`}
+                                            href={`/articles/${post.slug}`}
                                             className={classNames(
                                               active ? 'bg-gray-100' : '',
                                               'block px-4 py-2 text-sm text-gray-700'
